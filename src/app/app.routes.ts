0bcecd66b01c128714fd00c5b5f5d1483cb2535f
Zach Legesse <zachlegesse@gmail.com>
2024-08-25 16:02:49 -0400

    move to angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { FirstWorkTermReport } from '../app/work-term-report-1/first-work-term-report.component'
+
+export const routes: Routes = [{path:'firstWorkTermReport', component: FirstWorkTermReport}];
---
+++ src/app/work-term-report-1/first-work-term-report.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-work-term-report',
+  standalone: true,
+  templateUrl: './first-work-term-report.component.html',
+})
+export class FirstWorkTermReport {
+  title = 'portfolio-site';
+}
