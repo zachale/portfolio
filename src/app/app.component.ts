64b0e36f9e6c486ffcd36bc5f86c999dfd64cdca
Zach Legesse <zachlegesse@gmail.com>
2024-08-28 17:40:44 -0400

    added prettier, tailwind, and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import { NavBarModule } from './nav-bar/nav-bar.module';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports:[FlexLayoutModule, RouterOutlet, NavBarModule, NzButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/nav-bar/nav-bar.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { NavBar } from "./nav-bar.component";
+
+
+@NgModule ({
+  declarations:[NavBar],
+  exports: [NavBar]
+}) 
+export class NavBarModule{}
