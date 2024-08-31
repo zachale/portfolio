71e3c906b0ab87fb052d1050ec23c059b5d97f67
Zach Legesse <47370318+zachale@users.noreply.github.com>
2024-08-31 10:34:08 -0400

    Revert "Updating to Angular"

--- src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { FirstWorkTermReport } from '../app/work-term-report-1/first-work-term-report.component'
-
-export const routes: Routes = [{path:'', loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule)},{path:'firstWorkTermReport', component: FirstWorkTermReport}];
--- src/app/work-term-report-1/first-work-term-report.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { FirstWorkTermReport } from "./first-work-term-report.component";
-
-
-@NgModule ({
-  declarations: [FirstWorkTermReport],
-  exports: [FirstWorkTermReport]
-}) 
-export class FirstWorkTermReportModule{}
