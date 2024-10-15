6e08cff4106040f0772c2fe7b23bea595b0e74de
Zach Legesse <47370318+zachale@users.noreply.github.com>
2024-10-15 18:20:16 -0400

    Fixed posthog integration #11 (#14)
    * Fixed posthog integration #11
    
    * Added support to keep tabs on all users regardless of identification #11

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { posthog } from 'posthog-js';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
+
+posthog.init('phc_jP728LsaPTymnxkXfXgM0YBR93LcNtL2KmLiyoUfSVS', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'always',
+});
