71f4daa73084dd234bea915ca38774498efc6969
numba19 <zachlegesse@gmail.com>
2024-12-23 17:23:32 -0500

    Updated nav bar #17

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Location } from "@angular/common";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss',
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrl: "./nav-bar.component.scss",
 })
 export class NavBar {
   public innerWidth: any;
+  constructor(private location: Location, private router: Router) {}
   ngOnInit() {
     this.innerWidth = window.innerWidth;
+  }
+  navigateTo(route: string) {
+    if (this.router.url === route) {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    } else {
+      this.location.replaceState(route);
+    }
   }
 }
--- src/app/nav-bar/nav-bar.module.ts
+++ src/app/nav-bar/nav-bar.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from "@angular/core";
 import { NavBar } from "./nav-bar.component";
-import { NzIconModule  } from 'ng-zorro-antd/icon';
-import { MatButtonModule } from '@angular/material/button'
-import { MatCardModule } from '@angular/material/card'
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatIconModule } from "@angular/material/icon";
 
-
-@NgModule ({
-  declarations:[NavBar],
-  imports: [NzIconModule, MatButtonModule, MatCardModule, FlexLayoutModule ],
-  exports: [NavBar]
-}) 
-export class NavBarModule{}
+@NgModule({
+  declarations: [NavBar],
+  imports: [
+    NzIconModule,
+    MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
+  ],
+  exports: [NavBar],
+})
+export class NavBarModule {}
