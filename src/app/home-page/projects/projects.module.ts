e75e3142550d3f32033ce7afb74db713f4a020dc
numba19 <zachlegesse@gmail.com>
2024-12-23 20:01:44 -0500

    Fixing regression #17

--- src/app/home-page/projects/projects.module.ts
+++ src/app/home-page/projects/projects.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { Projects } from "./projects.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { ProjectTileModule } from "../project-tile/project-tile.module";
+import { NzIconModule } from "ng-zorro-antd/icon";
 
 @NgModule({
   declarations: [Projects],
   imports: [
     FlexLayoutModule,
+    NzIconModule,
     ProjectTileModule,
   ],
   exports: [Projects],
