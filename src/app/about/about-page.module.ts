331752c1554b096021899a14f34656ff8a920741
numba19 <zachlegesse@gmail.com>
2024-12-28 22:00:52 -0500

    Added about page #19

--- src/app/about/about-page.module.ts
+++ src/app/about/about-page.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { AboutPage } from "./about-page.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { RouterModule } from "@angular/router";
+import { NzDividerModule } from "ng-zorro-antd/divider";
 
 @NgModule({
   declarations: [AboutPage],
   imports: [
     FlexLayoutModule,
+    NzDividerModule,
     RouterModule.forChild([{ path: "", component: AboutPage }]),
   ],
   exports: [AboutPage],
