8e9afb93776f42931b8afd253ecc00efd18ac481
zachale <zachlegesse@gmail.com>
2026-04-28 13:36:38 -0400

    chore: update resume to F26_AI version

--- src/app/home-page/header/header.component.ts
+++ src/app/home-page/header/header.component.ts
@@ -8,7 +8,7 @@ export class Header {
   downloadResume() {
     // Create a temporary link element to trigger download
     const link = document.createElement('a');
-    link.href = '/assets/resume/Zach_Legesse_S26_2.pdf';
+    link.href = '/assets/resume/Zach_Legesse_F26_AI.pdf';
     link.download = 'Zach_Legesse_Resume.pdf';
     link.click();
   }
